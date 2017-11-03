70a992edd6958efd981892636c2f20c84dbc2f4f
yz <thiscallaname@gmail.com>
2017-11-03 14:47:29 +0800

    add some comment

--- simple_chat_room/chatRoom.erl
+++ simple_chat_room/chatRoom.erl
@@ -3,9 +3,10 @@
 
 
 start() ->
-	{ok, Listen} = gen_tcp:listen(6789, [binary, {packet, 4}, {reuseaddr, true}, {active, true}]),
+	{ok, Listen} = gen_tcp:listen(6789,
+					[binary, {packet, 4}, {reuseaddr, true}, {active, true}]),
 	register(master, self()),
-	spawn(chatRoom, connect, [Listen]),
+	spawn(chatRoom, connect, [Listen]), %创建新的进程负责监听端口
 	handle([]).
 
 connect(Listen) ->
@@ -16,8 +17,8 @@ connect(Listen) ->
 
 
 recv_message(Socket) ->
-	receive 
-		{tcp, Socket, Bin} -> 
+	receive
+		{tcp, Socket, Bin} ->
 			[Name,Str] = binary_to_term(Bin),
 			master!{message, Name, Str},
 			io:format("~p: ~p~n",[Name,Str]),
@@ -33,9 +34,11 @@ send_message([Socket | Set], Message) ->
 send_message([], Message) ->
 	done.
 
+% M进程，管理所有的服务器连接进程，某一个连接进程收到客户消息，发送给M进程
+% M进程再发送给所有客户，完成广播。
 handle(Set) ->
 	% io:format("~w: ~w~n",[self(), Set]),
-	receive 
+	receive
 		{new, Socket} ->
 			io:format("~w~n", [Socket]),
 			handle([Socket|Set]);
--- simple_chat_room/simpleClient.erl
+++ simple_chat_room/simpleClient.erl
@@ -3,14 +3,14 @@
 
 start(ClientName) ->
 	{ok, Socket} = gen_tcp:connect("localhost", 6789, [binary, {packet, 4}]),
-	spawn(?MODULE,loop,[Socket, ClientName, self()]),
+	spawn(?MODULE,loop,[Socket, ClientName, self()]), %开启loop新进程
 	print_message(Socket).
 
-loop(Socket, ClientName, Ppid) ->
+loop(Socket, ClientName, Ppid) -> %尾递归从控制台接收信息发送给服务器
 	Line = io:get_line("<<"),
 	Str = lists:delete(10, Line), %remove "\n" from the line.
 	% io:format("~p~n",[Str]),
-	if 
+	if
 		Str =:= "q" ->
 			gen_tcp:close(Socket),
 			Ppid ! {quit};
@@ -19,7 +19,7 @@ loop(Socket, ClientName, Ppid) ->
 			loop(Socket, ClientName, Ppid)
 	end.
 
-print_message(Socket) ->
+print_message(Socket) -> %打印从服务器接收到的信息
 	receive
 		{tcp, Socket, Bin} ->
 			[Name, Message] = binary_to_term(Bin),
@@ -27,5 +27,5 @@ print_message(Socket) ->
 			print_message(Socket);
 		{quit} ->
 			io:format("Bye!")
-	end.	
+	end.
 
